972c244e7e5dd34e3714d9c16db260d81dfef971
bitcorp <bitcorp@email.com>
2019-03-13 16:03:02 +0800

    Final format.bash

--- format.bash
+++ format.bash
@@ -3,10 +3,10 @@
 echo -n "Input filename: "
 read FILE
 
-StartOfLine=$(grep -n "Fast-forward" $FILE | cut -d: -f1 | wc -l)
+NoOfStart=$(grep -n "Fast-forward" $FILE | cut -d: -f1 | wc -l)
 CTR=1
 START=0
-VAR=$StartOfLine
+VAR=$NoOfStart
 while [ $VAR -ne 0 ] ; do
 	eval "START"$CTR=$(grep -n "Fast-forward" $FILE | cut -d: -f1 | sed -n $CTR"p")
 	(("START"$CTR++))
@@ -14,12 +14,12 @@ while [ $VAR -ne 0 ] ; do
 	((VAR--))
 done
 
-EndOfLine=$(grep "changed" $FILE | wc -l)
+EndOfLine=$(grep " changed" $FILE | wc -l)
 CTR=1
 END=0
 RAV=$EndOfLine
 while [ $RAV -ne 0 ] ; do
-	eval "END"$CTR=$(nl $FILE |  grep "changed" | tr -d " " | tr "\t" " " | cut -d" " -f1 | sed -n $CTR"p")
+	eval "END"$CTR=$(nl $FILE |  grep " changed" | tr -d " " | tr "\t" " " | cut -d" " -f1 | sed -n $CTR"p")
 	((CTR++))
 	((RAV--))
 done
@@ -27,7 +27,7 @@ done
 touch $(pwd)/output.txt
 echo " " > $(pwd)/output.txt
 CTR=1
-while [ $StartOfLine -ne 0 ] ; do
+while [ $NoOfStart -ne 0 ] ; do
 		if [ $CTR -eq 1 ] ; then
 			echo "----WEB-APP----" >> $(pwd)/output.txt
 		elif [ $CTR -eq 2 ] ; then
@@ -36,10 +36,9 @@ while [ $StartOfLine -ne 0 ] ; do
 	while [ $((START$CTR)) -ne $((END$CTR)) ] ; do
 		NUM=$((START$CTR))
 		sed -n $NUM"p" $FILE >> $(pwd)/output.txt
-	#	echo $(sed -n $NUM"p" $FILE) >> $(pwd)/output.txt
 		((START$CTR++))
 	done
-	((StartOfLine--))
+	((NoOfStart--))
 	((CTR++))
 done
 
